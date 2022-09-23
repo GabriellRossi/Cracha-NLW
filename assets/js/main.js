7b67b7aad572de24a054e078dcae6a438e5ec8aa
GabriellRossi <gabriel.rossi159@gmail.com>
2022-09-23 01:45:48 -0300

    Function to add links from dynamically added social networks.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
 const LinksSocialMedia = {
   github: 'GabriellRossi',
-  linkedin: 'gabriellrossi',
+  youtube: 'maykbrito',
   instagram: 'rossi_digital',
-  twitter: 'Gabriel__Rossi1',
   facebook: 'GBLRossi',
-  youtube: 'maykbrito'
+  twitter: 'Gabriel__Rossi1',
+  linkedin: 'gabriellrossi'
 }
 
-
 function changeSocialMediaLinks() {
-  for(let li of socialLinks.children) {
+  for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
 
     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
-    
   }
 }
 
